5c5b31503c7dcee4d819c6b634ec8d15df6a64f1
Gregory Szaro <gregoryszaro@gmail.com>
2023-02-18 20:39:28 -0500

    completes array lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+function appendCat(name) {
+    const moreCats = [...cats, name];
+    return moreCats;
+}
+function prependCat(name) {
+    const moreCats = [name, ...cats];
+    return moreCats;
+}
+function removeLastCat() {
+    const lessCats = cats.slice(0,cats.length-1);
+    return lessCats;
+}
+function removeFirstCat() {
+    const lessCats = cats.slice(1);
+    return lessCats;
+}
